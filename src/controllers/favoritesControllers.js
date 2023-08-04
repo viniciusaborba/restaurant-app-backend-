359dc9c578673bb692fd08363f75eb2c63ce0576
gustavo <gustavogarciaci1@gmail.com>
2023-08-04 18:37:20 -0300

    refactor favorites controller

--- src/controllers/favoritesControllers.js
+++ src/controllers/favoritesControllers.js
@@ -1,66 +1,59 @@
-const knex = require('../database/knex/connection')
-const AppError = require('../utils/AppError')
+const knex = require("../database/knex/connection");
+const AppError = require("../utils/AppError");
 
 class FavoritesControllers {
-    async create(req, res) {
-        const user_id = req.user.id
-        
-        const { name } = req.body
-        // return console.log(name)
+  async create(req, res) {
+    const user_id = req.user.id;
 
+    const { name } = req.body;
+    // return console.log(name)
 
-        const alreadyExists = await knex('favorites').where({ name }).first()
-        
-        if (alreadyExists) {
-            await knex('favorites').where({ name }).delete()
-            return res.status(200).json({message: 'Este prato já existe!'})
-        }
-        
-        if (!name) {
-            throw new AppError("Name is required", 400);
-        }
+    const alreadyExists = await knex("favorites").where({ name }).first();
 
-        await knex('favorites').insert({ name, user_id })
-        
-        return res.status(201).json({'message': 'Favorite created successfully' });
+    if (alreadyExists) {
+      await knex("favorites").where({ name }).delete();
+      return res.status(200).json({ message: "Este prato já existe!" });
     }
 
-    async index(req, res) {
-        const user_id = req.user.id
-
-        const favoriteList = await knex('favorites').where({ user_id })
-
-        return res.status(200).json(favoriteList)
+    if (!name) {
+      throw new AppError("Name is required", 400);
     }
 
-    async delete(req,res) {
-        const { name } = req.body
-        return console.log(name)
+    await knex("favorites").insert({ name, user_id });
 
+    return res.status(201).json({ message: "Favorite created successfully" });
+  }
 
-        if (!name) {
-            throw new AppError("Prato não encontrado.", 404)
-        }
-        
-        await knex('favorites').where({ name }).delete()
+  async index(req, res) {
+    const user_id = req.user.id;
 
-        return res.status(200).json({ message: 'Excluído com sucesso' })
+    const favoriteList = await knex("favorites").where({ user_id });
+
+    return res.status(200).json(favoriteList);
+  }
+
+  async delete(req, res) {
+    const { name } = req.query;
+
+    if (!name) {
+      throw new AppError("Prato não encontrado.", 404);
     }
 
-    async check(req, res) {
-        const { name } = req.body
+    await knex("favorites").where({ name }).delete();
 
-        // return console.log(name)
-        const isFavorite = await knex('favorites').where({ name }).first()
+    return res.status(200).json({ message: "Excluído com sucesso" });
+  }
 
-        if (isFavorite) {
-            return res.json({isFavorite: true})
-        } else {
-            return res.json({isFavorite: false})
+  async check(req, res) {
+    const { name } = req.query;
+    const isFavorite = await knex("favorites").where({ name }).first();
 
-        }
-
+    if (isFavorite) {
+      return res.json({ isFavorite: true });
+    } else {
+      return res.json({ isFavorite: false });
     }
+  }
 }
 
-module.exports = FavoritesControllers
+module.exports = FavoritesControllers;
